d0c12fad57c6852c283361cfaca17b356184da73
Kamar MEDDAH <kamar.meddah@gmail.com>
2021-02-14 19:10:01 +0100

    feat(modules): add shred and widget modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'folder/Inbox',
+    path: '**',
+    redirectTo: '/',
     pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
   }
 ];
 
