af602819686ed0f9aa51252e10d17d1635f54dc1
Kamar MEDDAH <kamar.meddah@gmail.com>
2021-02-22 04:59:36 +0100

    feat: add angular flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {BrowserModule, HammerModule} from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {AppComponent} from './app.component';
@@ -8,34 +8,22 @@ import {APP_BASE_HREF} from '@angular/common';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
 import {SharedModule} from './modules/shared/shared.module';
-import {LayoutModule} from './modules/layout/layout.module';
 import {CoreModule} from './core/core.module';
-import {LeafletModule} from '@asymmetrik/ngx-leaflet';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {IonicStorageModule} from '@ionic/storage';
-import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
-    SharedModule,
-    LayoutModule,
-    CoreModule,
-    LeafletModule,
-    FlexLayoutModule,
-    FormsModule,
-    ReactiveFormsModule,
     BrowserAnimationsModule,
-    IonicModule.forRoot(),
+    AppRoutingModule,
+    SharedModule,
+    CoreModule,
     IonicStorageModule.forRoot(),
-    HammerModule,
-    HttpClientModule,
+    IonicModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}),
   ],
   providers: [
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy},
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {LeafletModule} from '@asymmetrik/ngx-leaflet';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {LayoutModule} from '@angular/cdk/layout';
 
 
 @NgModule({
@@ -11,12 +13,16 @@ import {LeafletModule} from '@asymmetrik/ngx-leaflet';
     LeafletModule,
     FormsModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
+    LayoutModule,
   ],
   exports: [
     CommonModule,
     LeafletModule,
     FormsModule,
     ReactiveFormsModule,
+    FlexLayoutModule,
+    LayoutModule,
   ]
 })
 export class SharedModule {
